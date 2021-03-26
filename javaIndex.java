826d0525250b8fbd3e90aeb07513347c1bf7aedd
coderanam <coderanam@gamil.com>
2021-03-26 17:10:27 +0600

    Greetings Method added to javaIndex.java file

--- javaIndex.java
+++ javaIndex.java
@@ -1,4 +1,9 @@
 public class Hello {
     public static void main (string[] args){
         System.out.println("Vs Code is Awesome..");
+    }
+}
+
+public static void greeting(string name){
+    system.out.println("Welcom" + name);
 }
